4ca33b78de08d337f56cd9e6b6b6a8020355ea92
Estherr <queenstarry247@gmail.com>
2022-04-21 15:12:06 +0100

    vote service updated

--- src/services/vote.service.js
+++ src/services/vote.service.js
@@ -1,5 +1,23 @@
 class VoteService {
+    static async all() {
 
+    }
+
+    static async create(data) {
+
+    }
+
+    static async single(id) {
+
+    }
+
+    static async update(id, data) {
+
+    }
+    
+    static async delete(id) {
+
+    }
 }
 
 module.exports = VoteService
