c79bf8767da012a33acd5bd73ca16e8471fba3a5
oviecodes <alecgee73@gmail.com>
2022-05-19 11:52:43 +0100

    update

--- src/connectors/mongodb.js
+++ src/connectors/mongodb.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
 const { connect} = require("@interep/db")
 
-const database_url = process.env.MONGODB_URL || 'mongodb://localhost:27017/daocoders';
+// const database_url = process.env.MONGODB_URL || 'mongodb://localhost:27017/daocoders';
 
-// const database_url = process.env.DB_URL
+const database_url = process.env.DB_URL
 
-connect(database_url)
-.then(() => console.log('interep db connected successfully'))
-.catch((e) => console.log('Error with interep db connection'))
+// connect(database_url)
+// .then(() => console.log('interep db connected successfully'))
+// .catch((e) => console.log('Error with interep db connection'))
 
 mongoose.connect(database_url, {
     useNewUrlParser: true,
