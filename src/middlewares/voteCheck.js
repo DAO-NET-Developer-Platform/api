91fe0e0ae7475a06aaaab9473f17e1856d343fce
oviecodes <alecgee73@gmail.com>
2022-05-25 12:27:31 +0100

    update votes route

--- src/middlewares/voteCheck.js
+++ src/middlewares/voteCheck.js
@@ -27,6 +27,20 @@ module.exports = {
 
         return next()      
 
+    },
+
+    async getOrgId(req, res, next) {
+
+        const org = await organization.findBy('slug', req.params.organization_slug)
+
+        // console.log(org)
+
+        if(!org) return next(createError.NotFound('No such organization'))
+
+        req.params.organization_id = org._id
+
+        return next()
+
     }
 
 }
