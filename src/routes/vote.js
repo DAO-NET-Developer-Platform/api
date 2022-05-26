c9b49cc4736ab5ec2a7516b4e131063b2a89ef38
oviecodes <alecgee73@gmail.com>
2022-05-26 15:05:29 +0100

    update votes

--- src/routes/vote.js
+++ src/routes/vote.js
@@ -6,7 +6,7 @@ const globalCheck = require('../middlewares/check')
 const check = require('../middlewares/voteCheck')
 
 //vote
-routes.get('/:organization_slug', [check.getOrgId, globalCheck.getLang], vote.all)
+routes.get('/:organization_slug', [check.getOrgId, check.isUser, globalCheck.getLang], vote.all)
 routes.post('/:organization_slug', [validator(schemas.create), check.getOrgId, check.organization, check.user], vote.create)
 
 routes.get('/single/:id', [globalCheck.getLang], vote.single)
--- src/services/vote.service.js
+++ src/services/vote.service.js
@@ -2,19 +2,42 @@ const Vote = require('../models/Vote')
 const LanguageVote = require('../models/LanguageVote')
 const { uploadFile, retrieve } = require('../connectors/web3.storage')
 const language = require('../services/language.service')
+const Decision = require('../models/Decision')
 
 class VoteService {
 
-    static async all(id, language) {
+    static async all(id, language, user) {
 
         // console.log(language, id)
 
-        if(!language) return await Vote.find({organization: id}).lean()
+        if(!language) {
+            const vote = await Vote.find({organization: id}).lean()
+
+            await Promise.all(vote.map(async (el, i) => {
+                if(user) {
+                    const isVoted = await Decision.find({ $and: [{ vote: el._id, user }] }).lean()
+    
+                    vote[i].isVoted = !!isVoted.length
+                }
+                //check for percentage
+                if(vote.type == 'budget') {
+
+                } else {
+                    vote.percentage == null
+                }
+            }))
+
+            return vote
+        }
 
         const vote = await LanguageVote.find({ $and: [{ language, organization: id }] }).populate('vote').lean()
 
-        await Promise.all(vote.map((el, i) => {
+        await Promise.all(vote.map(async (el, i) => {
             vote[i].status = el.vote.status
+            if(user) {
+                const isVoted = await Decision.find({ $and: [{ vote: el.vote._id, user }] }).lean()
+                vote[i].isVoted = !!isVoted.length
+            }
         }))
 
         return vote
