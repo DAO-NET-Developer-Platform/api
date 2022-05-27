dc27c4004d48c3d222e407474b5000b0a673d03e
oviecodes <alecgee73@gmail.com>
2022-05-27 13:05:36 +0100

    update members route to use slug

--- src/routes/member.js
+++ src/routes/member.js
@@ -2,7 +2,7 @@ const routes = require('express').Router();
 const member = require('../controllers/member.controller');
 const check = require('../middlewares/memberCheck')
 
-routes.get('/:organization_id', member.getMembers)
-routes.post('/:organization_id/approve', [check.isMember, check.isPending, check.decided], member.approve)
+routes.get('/:organization_slug', check.getOrgId, member.getMembers)
+routes.post('/:organization_slug/approve', [check.getOrgId, check.isMember, check.isPending, check.decided], member.approve)
 
 module.exports = routes
